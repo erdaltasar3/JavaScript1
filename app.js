bdd755098779ad890dca823cdbd7d102a9c8ade4
erdaltasar3 <erdaltasar24@gmail.com>
2024-09-10 01:33:11 +0300

    Cikti alma metotlari

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Erdal Tasar");
+document.write("Erdal <br>");
+document.write("Tasar <br>");
+document.write("fenerbahce");
+
+
+let a = 10;
+let b = 5;
+console.log("a+b = "+(a+b));
+
+alert("2 sayinin toplami : "+(a+b));
