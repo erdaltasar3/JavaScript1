2e163516fc9b9a68a7c0398fd7b8b67077249902
erdaltasar3 <erdaltasar24@gmail.com>
2024-09-10 09:40:25 +0300

    dialog kutulari

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
+alert("Merhaba");
 
-// ! object
+let isim = prompt("İsminizi giriniz :");
+let yas = prompt("Yasinizi giriniz : ");
 
-let insan = {
-    isim:"Erdal",
-    yas:21,
-    id:1
-}
+console.log(isim);
+console.log(yas);
 
-console.log(insan);
 
-// ! List
-
-let dizi = [1,2,3,"f",true,5.2];
-
-console.log(typeof dizi);
-console.log(dizi);
-
-// ! function
-
-let func = function(){
-    console.log("Hello world");
-}
-
-console.log(typeof func);
-console.log(func);
+let result = confirm("Cikmak istediginize emin misiniz ?");
